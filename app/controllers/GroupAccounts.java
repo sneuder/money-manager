2c521e9c19e34a64da9dbb18eac75c879e8399fb
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-03-14 20:16:29 -0500

    feat: work api

--- app/controllers/GroupAccounts.java
+++ app/controllers/GroupAccounts.java
@@ -16,20 +16,18 @@ import models.accounts.GroupAccount;
 
 public class GroupAccounts {
   static MoneyManager database = App.database;
+  static Gson gson = new Gson();
 
   static public class ShowGroupAccounts implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
       ArrayList<GroupAccount> response = database.groupAccounts;
-      // exchange.sendResponseHeaders(200, response.size());
-      //
-      // Gson gson = new Gson();
-      // String json = gson.toJson(response);
+      String json = gson.toJson(response);
 
-      // exchange.sendResponseHeaders(200, json.getBytes().length);
+      exchange.sendResponseHeaders(200, json.getBytes().length);
 
       OutputStream os = exchange.getResponseBody();
-      // os.write(response.getBytes());
+      os.write(json.getBytes());
       os.close();
     }
   }
